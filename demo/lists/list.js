c888f01bcb9a0021a453e0b1b2635f019a80c5d6
caow <1322451414@qq.com>
2018-05-30 17:55:46 +0800

    修改list。js

--- demo/lists/list.js
+++ demo/lists/list.js
@@ -1,14 +1,36 @@
 $(function(){
+    //模拟ajax获取数据
+    var ret = [
+        ["canvas制作小游戏demo","2017-11-30"],
+        ["跳转页面打印方法研究","2017-12-17"],
+        ["小风车页面","2016-08-11"]
+    ];
+    var tr = "";
+    for(var i=0;i<ret.length;i++){
+        tr+=`<tr data-id="canvas">
+            <td>${ret[i][0]}</td>
+            <td>${ret[i][1]}</td>
+        </tr>`;
+    }
+    $(".lists table tbody tr").append(tr);
+
+    //模拟地址数据
+    var data_url = {
+        "canvas":"/page/index.html",
+        "print":"/page/index.html",
+        "xiaofenche":"/xiaofenche.html",
+    }
     $(".lists table tbody tr").on('click',function() {
         let data_id = $(this).attr("data-id");
         console.log(data_id);
-        if(data_id == "canvas"){
-            window.location.href = "/page/index.html";
+        if(data_url[data_id]){
+            window.location.href = data_url[data_id];
             return
-        }else if(data_id == "print"){
-            window.location.href = "/untile.html";
-            return
+        }else{
+            alert("地址访问错误！");
+            //后期添加404页面
         }
+        
 
     });
 });
